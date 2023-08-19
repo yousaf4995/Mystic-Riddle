ab7dee215f9136a58fd2b7eac1a185fe77439896
yousaf4995 <45572794+yousaf4995@users.noreply.github.com>
2023-08-20 00:33:19 +0500

    Grid Layout improvements

--- Assets/Scripts/GamePlay/Layout/CardGridLayout.cs
+++ Assets/Scripts/GamePlay/Layout/CardGridLayout.cs
@@ -3,12 +3,15 @@ using UnityEngine.UI;
 
 public class CardGridLayout : LayoutGroup
 {
-	public int gridRows;
-	public int gridColumns;
-	public Vector2 cellSize;
-	public Vector2 cellSpacing;
-	public int paddingFactor;
+    public int gridRows;
+    public int gridColumns;
+    public Vector2 cellSize;
+    public Vector2 cellSpacing;
+    public int paddingFactor;
 
+
+    [SerializeField] float parentWidth;
+    [SerializeField] float parentHeight;
     public override float minWidth => base.minWidth;
 
     public override float preferredWidth => base.preferredWidth;
@@ -24,61 +27,62 @@ public class CardGridLayout : LayoutGroup
     public override int layoutPriority => base.layoutPriority;
 
     public override void CalculateLayoutInputHorizontal()
-	{
-		base.CalculateLayoutInputHorizontal();
-		
-		if (gridRows == 0 || gridColumns == 0)
-		{
-			gridRows = 4;
-			gridColumns = 5;
-		}
+    {
+        base.CalculateLayoutInputHorizontal();
 
-		float parentWidth = rectTransform.rect.width;
-		float parentHeight = rectTransform.rect.height;
+        if (gridRows == 0 || gridColumns == 0)
+        {
+            gridRows = 4;
+            gridColumns = 5;
+        }
 
-		float cellHeight = (parentHeight - 2 * paddingFactor - (gridRows - 1) * cellSpacing.y) / gridRows;
-		float cellWidth = cellHeight;
+        parentWidth = rectTransform.rect.width;
 
-		cellSize.x = cellWidth;
-		cellSize.y = cellHeight;
+        parentHeight = rectTransform.rect.height;
+      
+        float cellHeight = (parentHeight - 2 * paddingFactor - cellSpacing.y * (gridRows - 1)) / gridRows;
+        float cellWidth = cellHeight;
 
-		padding.left = Mathf.FloorToInt((parentWidth - gridColumns * cellHeight) / 2);
-		padding.top = Mathf.FloorToInt((parentHeight - gridRows * cellWidth) / 2);
-		padding.bottom = padding.top;
+        if (cellWidth * gridColumns + cellSpacing.x * (gridRows - 1) > parentWidth)
+        {
+            cellWidth = (parentWidth - 2 * paddingFactor - cellSpacing.x* (gridColumns - 1)) / gridColumns;
+            cellHeight = cellWidth;
+        }
 
-		int columnCount = 0;
-		int rowCount = 0;
 
-		for (int i = 0; i < rectChildren.Count; i++)
-		{
-			rowCount = i / gridColumns;
-			columnCount = i % gridColumns;
+        cellSize.x = cellWidth;
+        cellSize.y = cellHeight;
 
-			var item = rectChildren[i];
+        padding.left = Mathf.FloorToInt((parentWidth - gridColumns * cellWidth - cellSpacing.x * (gridColumns - 1)) / 2);
+      //  padding.right = padding.left;
+        padding.top = Mathf.FloorToInt((parentHeight - gridRows * cellHeight - cellSpacing.y * (gridRows - 1)) / 2);
+     //   padding.bottom = padding.top;
 
-			var xPos = padding.left + (cellSize.x * columnCount) + (cellSpacing.x * (columnCount - 1));
-			var yPos = padding.top + (cellSize.y * rowCount) + (cellSpacing.y * (rowCount - 1));
+        int columnCount = 0;
+        int rowCount = 0;
 
-			SetChildAlongAxis(item, 0, xPos, cellSize.x);
-			SetChildAlongAxis(item, 1, yPos, cellSize.y);
-		}
-		
-	}
+        for (int i = 0; i < rectChildren.Count; i++)
+        {
+            rowCount = i / gridColumns;
+            columnCount = i % gridColumns;
+
+            var item = rectChildren[i];
+
+            var xPos = padding.left + (cellSize.x * columnCount) + (cellSpacing.x * (columnCount - 1));
+            var yPos = padding.top + (cellSize.y * rowCount) + (cellSpacing.y * (rowCount - 1));
+
+            SetChildAlongAxis(item, 0, xPos, cellSize.x);
+            SetChildAlongAxis(item, 1, yPos, cellSize.y);
+        }
+
+    }
+
 
     public override void CalculateLayoutInputVertical()
     {
-       
+
     }
 
-    public override bool Equals(object other)
-    {
-        return base.Equals(other);
-    }
-
-    public override int GetHashCode()
-    {
-        return base.GetHashCode();
-    }
 
     public override bool IsActive()
     {
@@ -87,57 +91,18 @@ public class CardGridLayout : LayoutGroup
 
     public override void SetLayoutHorizontal()
     {
-       
+
     }
 
     public override void SetLayoutVertical()
     {
-        
+
     }
 
-    public override string ToString()
-    {
-        return base.ToString();
-    }
-
-    protected override void Awake()
-    {
-        base.Awake();
-    }
 
     protected override void OnBeforeTransformParentChanged()
     {
         base.OnBeforeTransformParentChanged();
-    }
-
-    protected override void OnCanvasGroupChanged()
-    {
-        base.OnCanvasGroupChanged();
-    }
-
-    protected override void OnCanvasHierarchyChanged()
-    {
-        base.OnCanvasHierarchyChanged();
-    }
-
-    protected override void OnDestroy()
-    {
-        base.OnDestroy();
-    }
-
-    protected override void OnDidApplyAnimationProperties()
-    {
-        base.OnDidApplyAnimationProperties();
-    }
-
-    protected override void OnDisable()
-    {
-        base.OnDisable();
-    }
-
-    protected override void OnEnable()
-    {
-        base.OnEnable();
     }
 
     protected override void OnRectTransformDimensionsChange()
@@ -155,18 +120,4 @@ public class CardGridLayout : LayoutGroup
         base.OnTransformParentChanged();
     }
 
-    protected override void OnValidate()
-    {
-        base.OnValidate();
-    }
-
-    protected override void Reset()
-    {
-        base.Reset();
-    }
-
-    protected override void Start()
-    {
-        base.Start();
-    }
 }
